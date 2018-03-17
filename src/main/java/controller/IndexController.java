47051bb954ac0d3188abcc9a9dd37388c6cf9af3
eathon <yyye33@yahoo.com>
2018-03-17 20:33:19 +0800

    测试session

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,10 +1,39 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
-//    @RequestMapping("/index")
+
+    //添加一个日志器
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping(value = {"", "/", "/index"})
+    public ModelAndView getIndexView(HttpServletRequest request, HttpServletResponse response) {
+
+        ModelAndView mav = new ModelAndView();
+
+        HttpSession session = request.getSession();
+        Boolean loginSuccess = (Boolean) session.getAttribute("loginSuccess");
+        if (loginSuccess !=null && loginSuccess == true) {
+
+            //输出日志文件
+            logger.info("The index page.");
+
+            mav.setViewName("index");
+        } else {
+            mav.setViewName("login");
+        }
+
+        return mav;
+    }
 
 }
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -5,26 +5,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping(value = "/login")
 public class LoginController {
 
     //添加一个日志器
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    @RequestMapping({"/", "/index"})
-    public String index() {
-        //输出日志文件
-        logger.info("The index page.");
+    @RequestMapping("")
+    public ModelAndView getLoginView() {
+        ModelAndView modelAndView = new ModelAndView("login");
 
-
-        return "index";
+        return modelAndView;
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/do", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject login() {
+    public JSONObject login(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("username", "Eathon");
+        session.setAttribute("loginSuccess", true);
+
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("message", "success");
         return jsonObject;
